7c120587facca93f4cc16763246201330cf75a8e
Rodrigo Márquez <a01022943@itesm.mx>
2022-01-23 21:01:28 -0600

    lab7 intento1

--- Laboratorios/Lab7/script.js
+++ Laboratorios/Lab7/script.js
@@ -3,8 +3,10 @@ $(document).ready(function () {
   createButtons(animales);
 });
 
+// arreglo de animales
 let animales = ["dog", "cat", "bird", "cow", "tiger", "lion", "fox"];
 
+// funcion para crear botones iniciales
 function createButtons(aList) {
   aList.forEach((animal) => {
     let btn = $(`<button class="a-button">`);
@@ -13,14 +15,47 @@ function createButtons(aList) {
   });
 }
 
-$(document).on("click", ".a-button", function (e) {
-  e.preventDefault();
-  console.log($(this).text());
-});
-
+// agregar boton de animal con el formulario
 $(document).on("click", "#add-animal", function (e) {
   e.preventDefault();
   let btn = $(`<button class="a-button">`);
   btn.text($("#animal-input").val());
   $("#animal-buttons").append(btn);
 });
+
+// click en boton de animal
+$(document).on("click", ".a-button", function (e) {
+  e.preventDefault();
+  getGifs($(this).text());
+});
+
+function getGifs(aString) {
+  console.log(aString);
+  let peticion = {
+    url:
+      "https://api.giphy.com/v1/gifs/search?api_key=BF9ojDjo4vFsK2C4OmsdmjMbi3jeWb8O&q=" +
+      aString +
+      "&limit=10&offset=0&lang=en",
+    success: function (res) {
+      console.log(res.data);
+      $("#animals").empty();
+      res.data.forEach((gif) => {
+        item = $(`<div class ="animal-item">`);
+        rating = $(`<p>Rating: ${gif.rating}</p>`);
+        image = $("<img>");
+        image.attr("src", gif.images.fixed_height.url);
+        item.append(rating);
+        item.append(image);
+        $("#animals").append(item);
+        console.log(gif.rating);
+      });
+    },
+    error: function () {
+      console.log("Hubo un error");
+    },
+  };
+  $.ajax(peticion);
+}
+
+// API Key: BF9ojDjo4vFsK2C4OmsdmjMbi3jeWb8O
+// concatenar animal al url
